4abf7477f70f7d6297b84fa0892bf1c6da69d337
wangmeng <wangmeng@hztianque.com>
2019-03-16 12:29:57 +0800

    Spring annotation application add @Lazy @Bean 懒加载，不在IOC容器启动时加载，而是在第一次获取时加载，并初始化

--- springannotation/src/main/java/com/wangmeng/config/MainConfig2.java
+++ springannotation/src/main/java/com/wangmeng/config/MainConfig2.java
@@ -4,6 +4,7 @@ import com.wangmeng.bean.Person;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
@@ -22,11 +23,15 @@ public class MainConfig2 {
      * 	 以后每次获取就是直接从容器(map.get())中拿
      * 	 request:同一次请求创建一个实例
      * 	 session:同一个session创建创建一个实例
+     * 	 懒加载
+     * 	        单实例bean：默认在容器启动的时候创建对象；
+     * 	        懒加载：容器启动不创建对象，第一次使用（获取）Bean创建对象，并初始化；
      * @return
      */
     //默认是单实例
-    @Scope("prototype")
-    //@Scope
+   // @Scope("prototype")
+    @Scope
+    @Lazy
     @Bean("person")
     public Person person(){
         System.out.println("给容器中添加Person...");
